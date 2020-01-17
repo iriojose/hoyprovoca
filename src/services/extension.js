56cb33d385247466e8d7ff23c03f1b40fe277894
iriojose <iriojgomezv@gmail.com>
2020-01-17 10:06:25 -0400

    detalle

--- src/services/extension.js
+++ src/services/extension.js
@@ -1,7 +1,5 @@
-//extension
-//se puede poner local y por ip
 
-const extension1 = "192.168.0.253:81";
+//extension de la api
 const extension = process.env.VUE_APP_EXTENSION; 
 
 export default extension;
--- src/services/header.js
+++ src/services/header.js
@@ -1,6 +1,4 @@
 
 //header para acceder a la api ,(requerida)
-
-const header1=`{"password":"123456","user":"admin"}`;
 const header= process.env.VUE_APP_HEADER;
 export default header;
