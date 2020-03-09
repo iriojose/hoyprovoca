58e808166d295d5dc62a2490be52cfd40dcde92e
Duccem <teamlead@somossistemas.com>
2020-03-09 16:54:38 -0400

    fx

--- src/components/vistaAccount/OpcionesVistas.vue
+++ src/components/vistaAccount/OpcionesVistas.vue
@@ -26,12 +26,13 @@
                     { 
                         icon: "build", 
                         text: "Ajustes de cuenta", 
-                        path: "/account/profile" },
-                    {
+                        path: "/account/profile" 
+                    },
+                    /*{
                         icon: "credit_card",
                         text: "Metodos de pago",
                         path: "/account/metodo-de-pago"
-                    },
+                    },*/
                     {
                         icon: "calendar_today",
                         text: "Ultimos pedidos",
