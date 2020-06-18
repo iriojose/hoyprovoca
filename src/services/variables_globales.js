a019daa99383e6ef64e3711fd278576f77375f23
iriojose <iriojgomezv@gmail.com>
2020-06-18 09:38:37 -0400

    changes services

--- src/services/variables_globales.js
+++ src/services/variables_globales.js
@@ -5,5 +5,6 @@ const data      = process.env.VUE_APP_SERVICIO_DATA;
 const auth      = process.env.VUE_APP_SERVICIO_AUTH;
 const image     = process.env.VUE_APP_SERVICIO_IMAGES;
 const imagen    = process.env.VUE_APP_SERVICIO_IMAGEN;
+const tenant_id = process.env.VUE_APP_TENANT_ID;
 
-export default { image, data, auth, header, imagen, http };
+export default { image, data, auth, header, imagen, http,tenant_id };
