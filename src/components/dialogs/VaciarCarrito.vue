0a1138521f420bcb403078a09206119006379e23
iriojose <iriojgomezv@gmail.com>
2020-06-24 22:51:01 -0400

    fix modal delete all orders

--- src/components/dialogs/VaciarCarrito.vue
+++ src/components/dialogs/VaciarCarrito.vue
@@ -1,46 +1,31 @@
 <template>
-    <v-dialog v-model="dialogs" close-delay width="500" :persistent="loading" transition="dialog-bottom-transition"> 
-        <v-card width="100%" height="250" class="px-10 py-5">
-            <div class="text-center font-weight-black subtitle-1">
-                ¡Oopss!
-            </div>
-            <div class="text-center font-weight-black body-1 grey--text mt-5">
-                ¿estas seguro que quiere eliminar los pedidos del carrito?
-            </div>
+    <v-dialog v-model="dialogs" close-delay width="500" :persistent="loading" transition="dialog-bottom-transition">
+        <v-card width="100%" class="pa-5">
+            <v-card-title class="title font-weight-black">
+                <v-spacer></v-spacer>
+                    ¡Oopss!
+                <v-spacer></v-spacer>
+            </v-card-title>
+
             <v-card-text>
-                <v-row justify="center" align="center">
-                    <v-col cols="12" sm="5" md="5">
-                        <v-hover v-slot:default="{hover}">
-                            <v-btn 
-                                :elevation="hover ? 3:1" 
-                                :text="hover ? false:true" 
-                                :color="hover ? '#005598':null"
-                                :dark="hover ? true:false"
-                                class="text-capitalize"
-                                :disabled="loading"
-                                block
-                                @click="close()"
-                            >
-                                No, volver
-                            </v-btn>
-                        </v-hover>
-                    </v-col>
-                    <v-col cols="12" sm="5" md="5">
-                        <v-hover v-slot:default="{hover}">
-                            <v-btn 
-                                :elevation="hover ? 3:1" 
-                                :text="hover ? false:true" 
-                                :color="hover ? '#005598':null"
-                                :dark="hover ? true:false"
-                                class="text-capitalize"
-                                block
-                                :loading="loading"
-                                @click="adelete()"
-                            >
-                                Sí, seguro
-                            </v-btn>
-                        </v-hover>
-                    </v-col>
+                <div class="text-center font-weight-black subtitle-1 grey--text my-5">
+                    ¿estas seguro que quiere eliminar los pedidos del carrito?
+                </div>
+
+                <v-row justify="space-around">
+                    <v-btn 
+                        class="text-capitalize font-weight-bold" 
+                        :disabled="loading" @click="close()"
+                    >
+                        No, Volver
+                    </v-btn>
+
+                    <v-btn 
+                        :loading="loading" color="#0f2441" @click="deleteAll()"
+                        class="white--text text-capitalize font-weight-bold"
+                    >
+                        Sí, seguro
+                    </v-btn>
                 </v-row>
             </v-card-text>
         </v-card>
@@ -71,15 +56,17 @@ import Pedidos from '@/services/Pedidos';
             close(){
                 this.setModalCarrito(false);
             },
-            adelete(){
+            deleteAll(){
                 this.loading = true;
-                this.pedidos.filter(a => this.deletePedidos(a.id));
-                this.deleteCarrito();
-                this.loading = false;
+                this.pedidos.filter((a,i) => this.deletePedidos(a.id,i));
             },
-            deletePedidos(id){
-                Pedidos().delete(`/${id}`).then((response) => {
-                    this.close();
+            deletePedidos(id,i){
+                Pedidos().delete(`/${id}`).then(() => {
+                    if(i == this.pedidos.lenght -1){
+                        this.deleteCarrito();
+                        this.loading = false;
+                        this.close();
+                    }
                 }).catch(e => {
                     console.log(e);
                 });
